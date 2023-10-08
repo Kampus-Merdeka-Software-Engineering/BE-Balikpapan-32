098485e45e093ceb2a82f3d48fb1d26a2de9eb57
Denilson Aji Santoso <57387034+DenilsonSantoso17@users.noreply.github.com>
2023-10-08 07:43:08 +0700

    update rute dan deployment

--- prisma/migrations/20231007125506_initialitations/migration.sql
+++ prisma/migrations/20231007125506_initialitations/migration.sql
@@ -13,7 +13,7 @@ CREATE TABLE `Pendaftaran` (
 CREATE TABLE `Feedback` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `nama` VARCHAR(191) NOT NULL,
-    `pesan` VARCHAR(300) NOT NULL,
+    `pesan` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
